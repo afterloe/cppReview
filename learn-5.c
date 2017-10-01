e1c71b911e433c682babbd230cb5fcd5c1b98414
afterloe <lm6289511@gmail.com>
2017-10-01 21:30:54 +0800

    修改期化年利率数值

--- learn-5.c
+++ learn-5.c
@@ -5,10 +5,10 @@
 #include<stdio.h>
 
 int main() {
- float r1 = 0.036, r2 = 0.051, r3 = 0.042, principal;
+ float r1 = 0.0036, r2 = 0.0225, r3 = 0.0198, principal;
  scanf("%f", &principal);
  printf("存活期一年后获得的本金和利息为: %f \n", principal * (1 + r1));
  printf("存定期一年后获得的本金和利息为: %f \n", principal * (1 + r2));
- printf("存两次半年定期获得的本金和利息为: %f \n", (principal + principal * r3 / 2) * ( 1 + r3 / 2) );
+ printf("存两次半年定期获得的本金和利息为: %f \n", principal * (1 + r3 / 2) * (1 + r3 / 2) );
  return 0;
 }
