661457aa60411de823b1677a43dd010ec7b1e971
afterloe <lm6289511@gmail.com>
2017-10-02 22:47:31 +0800

    实现指针版冒泡排序

--- learn-9.c
+++ learn-9.c
@@ -1,36 +1,22 @@
-/**
- *   输入5个学生的数据，然后按照平均成绩排序，最后输出成绩最高的学生和成绩最低的学生
- *   
- * create by afteloe, on 2017-10-02
- */
 #include<stdio.h>
-#define N 5
-
-struct Student {
-  int num;
-	char name[20];
-	float source[3];
-	float aver;
-}
 
 int main() {
-	void inputStudentInfo(struct Student * students_p);
-	void sortByAver(struct Student * students_p);
-	void toString(struct Student * student);
-
-	struct Student students[N], * students_p = students, * student;
-	inputStudentInfo(students_p);
-	sortByAver(students_p);
-	student = students;
-	toString(student);
-	student = students + N - 1;
-	toString(student);
-  return 0;
-}
-
-void inputStudentInfo(struct Student * students_p) {
-  for (int i = 0; i < N; i++) {
-	  printf("please input no.%d 's info", i + 1);
-		scanf("%d %s %f %f %f", &* (students_p + i) -> num);
+	int array[] = {23,34,12,32,76,34,89,223,14,12}, leng;
+	leng = sizeof(array) / sizeof(int); //  sizeof(数组名) 得到这个数组占用内存的总长度  公式：  sizeof(数组名) / sizeof(数组类型);
+	printf("array size is %d \n", leng);
+	int *a, *b;
+	for (a=array; a < (array + leng); a++) {
+		for (b=array; b < a; b++) {
+		  if (*a < *b) {
+				*a = *a + *b;
+				*b = *a - *b;
+				*a = *a - *b;
+			}
+		}
 	}
+	for (a = array; a < (array + leng); a++) {
+	  printf("%d ", *a);
+	}
+	printf("\n");
+  return 0;
 }
