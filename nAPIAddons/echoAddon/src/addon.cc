569fca316db0a6078fa3f73dd601b7ae236bf69e
afterloe <lm6289511@gmail.com>
2019-09-20 10:19:57 +0800

    fix: 修复因为疏忽导致的函数编译异常

--- nAPIAddons/echoAddon/src/addon.cc
+++ nAPIAddons/echoAddon/src/addon.cc
@@ -1,24 +1,29 @@
 // Function with argument - n-api
-
 #include <napi.h>
 
+/**
+ *	回声函数扩展
+ *	
+ *	node_module 安装 node-addon-api 模块， 同时binding.gyp添加对该模块的配置，否则无法使用napi.h内的封装类 
+ */ 
 namespace echo {
 
 	Napi::Value Echo(const Napi::CallbackInfo& args) {
 		Napi::Env env = args.Env();
 
-		if (0 == args.length()) {
+		if (0 == args.Length()) {
 			Napi::TypeError::New(env, "必须输入arguments!")
 				.ThrowAsJavaScriptException();
 			return env.Null();
 		}
 
-		string receive = args[0].As<Napi::String>().Utf8Value();
+		std::string receive = args[0].As<Napi::String>().Utf8Value();
 		return Napi::String::New(env, receive);
 	}
 
-	Napi::Object Init(Napi::Env env, Napi::Object exception) {
+	Napi::Object Init(Napi::Env env, Napi::Object exports) {
 		exports.Set(Napi::String::New(env, "echo"), Napi::Function::New(env, Echo));
+		return exports;
 	}
 
 	NODE_API_MODULE(addon, Init);
