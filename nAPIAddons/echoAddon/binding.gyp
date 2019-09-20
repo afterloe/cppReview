a194cc695869085196a34f84ce3853cd421a1d1d
afterloe <lm6289511@gmail.com>
2019-09-20 09:28:36 +0800

    fix: napi 工具包改写

--- nAPIAddons/echoAddon/binding.gyp
+++ nAPIAddons/echoAddon/binding.gyp
@@ -2,7 +2,13 @@
 	"targets": [
 		{
 			"target_name": "echo",
-			"sources": ["src/addon.cc"]
+			"sources": ["src/addon.cc"],
+			"cflags!": [ "-fno-exceptions" ],
+      "cflags_cc!": [ "-fno-exceptions" ],
+      "defines": [ "NAPI_DISABLE_CPP_EXCEPTIONS" ],
+			"include_dirs": [
+				"<!@(node -p \"require('node-addon-api').include\")"
+			]
 		}
 	]
 }
--- nAPIAddons/echoAddon/src/addon.cc
+++ nAPIAddons/echoAddon/src/addon.cc
@@ -1,23 +1,11 @@
 // Function with argument - n-api
 
 #include <napi.h>
-#include <std>
-
 
 namespace echo {
 
-	template<typename OS, typename T> void ostr(OS& o, T t) {
-		o << t;
-	}
-
-	template<typename... ARG> auto argcat(ARG... arg) -> string {
-		ostringstream os;
-		int arr[] = {(ostr(os, arg), 0)...};
-		return os.str();
-	}
-
 	Napi::Value Echo(const Napi::CallbackInfo& args) {
-		Napi::Env env = args.env();
+		Napi::Env env = args.Env();
 
 		if (0 == args.length()) {
 			Napi::TypeError::New(env, "必须输入arguments!")
@@ -26,12 +14,12 @@ namespace echo {
 		}
 
 		string receive = args[0].As<Napi::String>().Utf8Value();
-		return Napi::String::New(env, argcat("hahah, you say -> ", receive));
+		return Napi::String::New(env, receive);
 	}
 
 	Napi::Object Init(Napi::Env env, Napi::Object exception) {
 		exports.Set(Napi::String::New(env, "echo"), Napi::Function::New(env, Echo));
 	}
 
-	NODE_API_MODULE(addon, Init)
+	NODE_API_MODULE(addon, Init);
 }
